7a6692a8fb34d4c2e41523a1cc766f2d976aaac4
Dmitry Fomin <fomin.dm@gmail.com>
2025-12-10 21:38:05 +0100

    Day 10

--- src/day10.rs
+++ src/day10.rs
@@ -1,4 +1,6 @@
-use ahash::{HashSet, HashSetExt};
+use std::i64;
+
+use ahash::{HashMap, HashMapExt, HashSet, HashSetExt};
 use rayon::{iter::ParallelIterator, str::ParallelString};
 
 fn process_line(line: &[&str]) -> i64 {
@@ -15,26 +17,24 @@ fn process_line(line: &[&str]) -> i64 {
                 .collect()
         })
         .collect();
-    let mut values = vec![(vec![false; target.len()], vec![0; target.len()])];
-    let mut used: HashSet<(Vec<bool>, Vec<i64>)> = HashSet::new();
+    let mut values = vec![vec![false; target.len()]];
+    let mut used: HashSet<Vec<bool>> = HashSet::new();
     used.insert(values[0].clone());
     let mut step = 0;
     while !values.is_empty() {
-        let mut new_values: Vec<(Vec<bool>, Vec<i64>)> = vec![];
-        for (value, cur_joltage) in &values {
+        let mut new_values: Vec<Vec<bool>> = vec![];
+        for value in &values {
             if *value == target {
                 return step;
             }
             for op in &ops {
                 let mut new_value = value.clone();
-                let mut new_joltage = cur_joltage.clone();
                 for &index in op {
                     new_value[index] = !new_value[index];
                 }
-                let new_record = (new_value, new_joltage.clone());
-                if !used.contains(&new_record) {
-                    new_values.push(new_record.clone());
-                    used.insert(new_record);
+                if !used.contains(&new_value) {
+                    new_values.push(new_value.clone());
+                    used.insert(new_value);
                 }
             }
         }
@@ -42,6 +42,84 @@ fn process_line(line: &[&str]) -> i64 {
         step += 1;
     }
     -1
+}
+
+fn split(n: i64, count: usize, cache: &mut HashMap<(i64, usize), Vec<Vec<i64>>>) -> Vec<Vec<i64>> {
+    if cache.contains_key(&(n, count)) {
+        return cache[&(n, count)].clone();
+    }
+    let mut result = vec![];
+    for i in 0..n + 1 {
+        if n - i == 0 && count - 1 == 0 {
+            result.push(vec![i]);
+        } else if count - 1 > 0 {
+            let sub = split(n - i, count - 1, cache);
+            for mut v in sub {
+                v.push(i);
+                result.push(v);
+            }
+        }
+    }
+    cache.insert((n, count), result.clone());
+    result
+}
+
+fn count_joltage(
+    target: &mut Vec<i64>,
+    ops: Vec<Vec<usize>>,
+    cur: i64,
+    cache: &mut HashMap<(i64, usize), Vec<Vec<i64>>>,
+) -> i64 {
+    if target.iter().sum::<i64>() == 0 {
+        return cur;
+    }
+    if ops.is_empty() {
+        return i64::MAX;
+    }
+    let mut ops_for_target = vec![0; target.len()];
+    for op in &ops {
+        for &index in op {
+            ops_for_target[index] += 1;
+        }
+    }
+    let ops_indices: Vec<(i64, usize)> = ops_for_target
+        .iter()
+        .enumerate()
+        .filter(|(_, v)| **v > 0)
+        .map(|(i, &v)| (v, i))
+        .collect();
+    let best_index = ops_indices.iter().min().unwrap().1;
+    let mut use_ops_indices = vec![];
+    let mut result = i64::MAX;
+    for (i, op) in ops.iter().enumerate() {
+        if op.contains(&best_index) {
+            use_ops_indices.push(i);
+        }
+    }
+    for comb in split(target[best_index], use_ops_indices.len(), cache) {
+        for (c, &op_index) in comb.iter().zip(use_ops_indices.iter()) {
+            for &index in &ops[op_index] {
+                target[index] -= c;
+            }
+        }
+        let v = cur + comb.iter().sum::<i64>();
+        if target.iter().all(|&x| x >= 0) && v < result {
+            let mut new_ops = vec![];
+            for (i, op) in ops.iter().enumerate() {
+                if !use_ops_indices.contains(&i) {
+                    new_ops.push(op.clone());
+                }
+            }
+            let sub = count_joltage(target, new_ops, v, cache);
+            result = result.min(sub);
+        }
+        for (c, &op_index) in comb.iter().zip(use_ops_indices.iter()) {
+            for &index in &ops[op_index] {
+                target[index] += c;
+            }
+        }
+    }
+    result
 }
 
 pub fn part1(input: &str) -> i64 {
@@ -62,8 +140,23 @@ pub fn part2(input: &str) -> i64 {
         .map(|line| line.trim().split_whitespace().collect::<Vec<_>>())
         .collect::<Vec<_>>();
     let mut result = 0;
+    let mut cache = HashMap::new();
     for line in lines {
-        result += process_line(&line);
+        let mut target: Vec<i64> = line[line.len() - 1][1..line[line.len() - 1].len() - 1]
+            .split(",")
+            .map(|v| v.parse().unwrap())
+            .collect::<Vec<_>>();
+        let ops: Vec<Vec<usize>> = line[1..line.len() - 1]
+            .iter()
+            .map(|part| {
+                part[1..part.len() - 1]
+                    .split(",")
+                    .map(|v| v.parse::<usize>().unwrap())
+                    .collect()
+            })
+            .collect();
+        let sub = count_joltage(&mut target, ops, 0, &mut cache);
+        result += sub;
     }
     result
 }
