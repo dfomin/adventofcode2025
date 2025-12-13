c48416850cb36e62e1f1aae9ffd8ec1c760a07da
Dmitry Fomin <fomin.dm@gmail.com>
2025-12-13 11:09:48 +0100

    Refactoring day 1

--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,152 @@
+use adventofcode2025::{Day, day1::Day1};
+
 fn main() {
     let mut day = 1;
     let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day1::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day1::part2(&input)
-    );
+    let day1 = Day1::new(&input);
+    println!("Day {} part 1: {}", day, day1.part1());
+    println!("Day {} part 2: {}", day, day1.part2());
 
-    day = 2;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day2::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day2::part2(&input)
-    );
+    // day = 2;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day2::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day2::part2(&input)
+    // );
 
-    day = 3;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day3::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day3::part2(&input)
-    );
+    // day = 3;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day3::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day3::part2(&input)
+    // );
 
-    day = 4;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day4::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day4::part2(&input)
-    );
+    // day = 4;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day4::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day4::part2(&input)
+    // );
 
-    day = 5;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day5::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day5::part2(&input)
-    );
+    // day = 5;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day5::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day5::part2(&input)
+    // );
 
-    day = 6;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day6::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day6::part2(&input)
-    );
+    // day = 6;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day6::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day6::part2(&input)
+    // );
 
-    day = 7;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day7::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day7::part2(&input)
-    );
+    // day = 7;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day7::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day7::part2(&input)
+    // );
 
-    day = 8;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day8::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day8::part2(&input)
-    );
+    // day = 8;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day8::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day8::part2(&input)
+    // );
 
-    day = 9;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day9::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day9::part2(&input)
-    );
+    // day = 9;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day9::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day9::part2(&input)
+    // );
 
-    day = 10;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day10::part1(&input)
-    );
+    // day = 10;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day10::part1(&input)
+    // );
     // println!(
     //     "Day {} part 2: {}",
     //     day,
     //     adventofcode2025::day10::part2(&input)
     // );
 
-    day = 11;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day11::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day11::part2(&input)
-    );
+    // day = 11;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day11::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day11::part2(&input)
+    // );
 
-    day = 12;
-    let input = adventofcode2025::read_input(day);
-    println!(
-        "Day {} part 1: {}",
-        day,
-        adventofcode2025::day12::part1(&input)
-    );
-    println!(
-        "Day {} part 2: {}",
-        day,
-        adventofcode2025::day12::part2(&input)
-    );
+    // day = 12;
+    // let input = adventofcode2025::read_input(day);
+    // println!(
+    //     "Day {} part 1: {}",
+    //     day,
+    //     adventofcode2025::day12::part1(&input)
+    // );
+    // println!(
+    //     "Day {} part 2: {}",
+    //     day,
+    //     adventofcode2025::day12::part2(&input)
+    // );
 }
